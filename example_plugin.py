ba5d7cfd44ef800f49f3e319ea376e7af5e7484d
Jeremiah K <17190268+jeremiah-k@users.noreply.github.com>
2024-11-24 16:33:46 -0600

    First commit

---
+++ example_plugin.py
@@ -0,0 +1,15 @@
+from plugins.base_plugin import BasePlugin
+
+class Plugin(BasePlugin):
+    plugin_name = "example_plugin"
+
+    async def handle_meshtastic_message(self, packet, formatted_message, longname, meshnet_name):
+        # Check if the packet is a TEXT_MESSAGE_APP packet
+        if "decoded" in packet and "portnum" in packet["decoded"]:
+            if packet["decoded"]["portnum"] == "TEXT_MESSAGE_APP":
+                self.logger.debug("Debug logging on Meshtastic TEXT_MESSAGE_APP message")
+        else:
+            self.logger.debug("Non-TEXT_MESSAGE_APP packet received. Ignoring.")
+
+    async def handle_room_message(self, room, event, full_message):
+        self.logger.debug("Debug logging on Matrix message")
